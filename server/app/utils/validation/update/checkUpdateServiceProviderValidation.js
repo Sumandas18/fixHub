481f94a640e89ec973da708b631bb5e25bbc9499
Suman-da97 <sumandass1811@gmail.com>
2026-04-16 02:25:48 +0530

    fix(validation): rename validation object for service provider updates and include additional optional fields

--- server/app/utils/validation/update/checkUpdateServiceProviderValidation.js
+++ server/app/utils/validation/update/checkUpdateServiceProviderValidation.js
@@ -1,8 +1,11 @@
 const JOI = require("joi");
 
-const updateServiceValidate = JOI.object({
+const updateServiceProviderValidate = JOI.object({
+    service_area_zip: JOI.string().optional(),
+    experience: JOI.alternatives().try(JOI.string(), JOI.number()).optional(),
+    charges_per_hour: JOI.alternatives().try(JOI.string(), JOI.number()).optional(),
     service_name: JOI.string().optional(),
     service_description: JOI.string().min(40).max(150).optional()
-}).min(1); 
+}).min(1);
 
-module.exports = updateServiceValidate;
+module.exports = updateServiceProviderValidate;
